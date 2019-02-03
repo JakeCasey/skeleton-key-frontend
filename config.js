2aad2c3efab7d537dbf7fb128e615a1b79b74058
Jake Casey <acasestudy0@gmail.com>
2019-02-02 18:50:04 -0600

    updated prod endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndPoint = `https://deva.io:23584`;
+export const prodEndPoint = `http://test-backend.deva.io`;
 export const perPage = 4;
