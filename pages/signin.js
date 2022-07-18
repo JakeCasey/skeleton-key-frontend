8c055e1dceca8fa14d50731912de404cfd2bd506
Jake Casey <acasestudy0@gmail.com>
2022-07-18 11:25:15 -0500

    user authentication w/ new prisma working

--- pages/signin.js
+++ pages/signin.js
@@ -4,10 +4,6 @@ import { NextSeo } from 'next-seo';
 
 const SignupPage = (props) => (
   <div>
-    <NextSeo
-      title="Dirigible Website Builder Login | Welcome Back, We Missed You"
-      description="Login to your Dirigible account. Dirigible is the website builder that grows and changes as your brand evolves."
-    />
     <Signin />
   </div>
 );
